8d3f5dd161de34b9f838041a6add77d53825b495
Argone <altirax@hotmail.com>
2026-09-20 14:41:34 +0200

    Add files via upload

--- boot.js
+++ boot.js
@@ -8,7 +8,7 @@
  */
 (function(){
   'use strict';
-  const FALLBACK_VERSION='20260916-0001';
+  const FALLBACK_VERSION='20260920-0001';
 
   function scriptUrl(file,version){
     return `${file}?v=${encodeURIComponent(version)}`;
